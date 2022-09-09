cf3c05966bf9ff66962a3bd8c3757f0cbd9665f6
Adarsh <112389532+Adrshcode@users.noreply.github.com>
2022-09-09 22:02:17 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    var socialLink = document.querySelector("#social-link");
+    var social = document.querySelector("#social").innerHTML;
+
+    socialLink.innerHTML = social;
+
+    var footData = document.querySelector("#foot-data").innerHTML;
+
+    
+
+}
+
+export var nav = document.querySelector("#nav").innerHTML;
+
+
+
---
+++ js/nav.js
@@ -0,0 +1,13 @@
+
+// window.onload = () => {
+//     alert("db")
+// }
+
+// alert("hg")
+  
+// import { nav } from './index'
+// alert("jwd");
+// var mainNav = nav;
+// var subNav = document.querySelector("#sub-nav");
+// subNav.innerHTML = mainNav;
+
